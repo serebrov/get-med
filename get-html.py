a2766367726dca416e2f6c3f78485782ff72b91c
seb <seb@seb-ubu-h.(none)>
2012-11-06 23:21:35 +0200

    Initial implementation

---
+++ get-html.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+import urllib
+
+# Handle command line options.
+if len(sys.argv) < 3:
+    print "Usage: python get.py list.csv outdir"
+    sys.exit(1)
+
+f = open(sys.argv[1], 'r')
+d = sys.argv[2]
+for url in f:
+    resp = urllib.urlopen(url)
+    fname = url.replace('http://','').replace('/','_').replace('\n', '.html')
+    out = open(d + '/' + fname, 'w')
+    out.write(resp.read())
+    print fname
